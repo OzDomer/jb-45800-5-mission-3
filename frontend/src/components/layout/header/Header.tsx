7abefa863d2cb662611339455236d0b492e6cff7
Oz Domer <ozdomer7@gmail.com>
2026-06-09 10:56:00 +0300

    test

--- frontend/src/components/layout/header/Header.tsx
+++ frontend/src/components/layout/header/Header.tsx
@@ -1,9 +1,11 @@
+import { NavLink } from "react-router-dom"
 import "./Header.css"
 
 export default function Header() {
     return (
         <div className="Header">
-
+            <NavLink to={'/'}>Home</NavLink>
+            <NavLink to={'/about'}>About</NavLink>
         </div>
     )
 }
--- frontend/src/components/layout/main/Main.tsx
+++ frontend/src/components/layout/main/Main.tsx
@@ -1,9 +1,15 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Home from "../../home/Home";
+import About from "../../about/About";
 
 export default function Main() {
     return (
         <Routes>
-            {/* add <Route /> as needed */}
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            {/* <Route path="/meetings" element={<Meetings />} />
+            <Route path="/add-meeting" element={<AddMeeting />} />
+            <Route path="/edit-meeting/:meetingId" element={<EditMeeting />} /> */}
         </Routes>
     )
 }
