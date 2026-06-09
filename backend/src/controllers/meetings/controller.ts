be6d776641849379d4421f1c0bb641295813c28b
Oz Domer <ozdomer7@gmail.com>
2026-06-09 10:04:38 +0300

    new meeting enpoint done

--- backend/src/controllers/meetings/controller.ts
+++ backend/src/controllers/meetings/controller.ts
@@ -13,3 +13,32 @@ export async function getMeetinsPerTeamId(request: Request<{ teamId: string }>, 
         next(e)
     }
 }
+export async function getMeetingById(request: Request<{ meetingId: string }>, response: Response, next: NextFunction) {
+    try {
+        const { meetingId } = request.params
+
+        const meeting = await Meeting.findByPk(meetingId)
+        if (!meeting) return next(
+            {
+                status: 404,
+                message: 'Meeting not found'
+            })
+
+        response.json(meeting)
+    } catch (e) {
+        next(e)
+    }
+}
+export async function newMeeting(request: Request<{}, {}, { teamId: string, startTime: Date, endTime: Date, description: string, room: string}>, response: Response, next: NextFunction) {
+    try {
+        const newMeeting = await Meeting.create({
+            ...request.body
+        })
+        response.json(newMeeting)
+    } catch (e) {
+        next(e)
+    }
+}
+
+
+
--- backend/src/routers/meetings.ts
+++ backend/src/routers/meetings.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getMeetinsPerTeamId } from "../controllers/meetings/controller";
+import { getMeetingById, getMeetinsPerTeamId, newMeeting } from "../controllers/meetings/controller";
 
 
 const meetingsRouter = Router()
 
 
-meetingsRouter.get('/:teamId', getMeetinsPerTeamId)
+meetingsRouter.get('/by-team/:teamId', getMeetinsPerTeamId)
+meetingsRouter.get('/:meetingId', getMeetingById)
+meetingsRouter.post('/', newMeeting)
 
 
 
